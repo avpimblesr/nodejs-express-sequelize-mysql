1004c480c0055507d60624ebe3ea66f6a6c29617
avpimblesr <allan.pimble@gmail.com>
2020-10-31 15:18:42 -0400

    Fleshed out create and find methods in the controller.

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -4,17 +4,62 @@ const Op = db.Sequelize.Op;
 
 // Create and Save a new Tutorial
 exports.create = (req, res) => {
-
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({
+      message: 'Content can not be empty!'
+    });
+    return;
+  }
+  // Create a Tutorial
+  const tutorial = {
+    title: req.body.title,
+    description: req.body.description,
+    published: req.body.published ? req.body.published : false
+  };
+  // Save Tutorial in the database
+  Tutorial.create(tutorial)
+    .then(date => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || 'Some error occurred while creating the Tutorial.'
+      });
+    });
 };
 
 // Retrieve all Tutorials from the database
 exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
 
+  Tutorial.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || 'Some error occurred while retrieving tutorials.'
+      });
+    });
 };
 
 // Find a single Tutorial with an id
 exports.findOne = (req, res) => {
+  const id = req.params.id;
 
+  Tutorial.findByPk(id)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: 'Error retrieving Tutorial with id= ' + id
+      });
+    });
 };
 
 // Update a Tutorial by the id in the request
