1aaccffbc26847e9cc054b84a29da2582c13afc2
avpimblesr <allan.pimble@gmail.com>
2020-10-31 15:03:11 -0400

    Created the tutorial model and framed the tutorial controller

---
+++ app/controllers/tutorial.controller.js
@@ -0,0 +1,34 @@
+const db = require('../models');
+const Tutorial = db.tutorials;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new Tutorial
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all Tutorials from the database
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single Tutorial with an id
+exports.findOne = (req, res) => {
+
+};
+
+// Update a Tutorial by the id in the request
+exports.updates = (req, res) => {
+
+};
+
+// Delete all Tutorials from the database
+exports.deleteAll = (req, res) => {
+
+};
+
+// Find all published Tutorials
+exports.findAllPublished = (req, res) => {
+
+};
+
